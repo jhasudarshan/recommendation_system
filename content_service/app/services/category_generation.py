e574b4a8a0e5b95efde9ecf709b09d5bd4744b09
jhasudarshan <jha.microbit@gmail.com>
2025-04-10 22:12:14 +0530

    cache-model-check-before-download-online

--- content_service/app/services/category_generation.py
+++ content_service/app/services/category_generation.py
@@ -11,11 +11,13 @@ from app.config.config import CLASSIFIER_MODEL,CLASSIFICATION,CONTENT_CLASSIFY_T
 from app.event.kafka_consumer import KafkaEventConsumer
 from app.config.logger_config import logger
 from app.event.kafka_service import content_kafka_service
+import os
 
 class ContentClassifier:
     def __init__(self, model_name: str, classification_type: str, confidence_threshold: float = 0.7):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        hf_cache_dir = os.getenv("HF_HOME", "/content_service/hf_cache")
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name,cache_dir=hf_cache_dir)
+        model = AutoModelForSequenceClassification.from_pretrained(model_name,cache_dir=hf_cache_dir)
 
         # Dynamic Quantization for CPU optimization
         self.model = torch.quantization.quantize_dynamic(
@@ -23,6 +25,7 @@ class ContentClassifier:
         )
     
         self.model.eval()  # VERY IMPORTANT
+        self.model.to("cpu")
         self.confidence_threshold = confidence_threshold
 
         self.categories = [
@@ -31,19 +34,23 @@ class ContentClassifier:
         ]
 
     def classify(self, texts: List[str]) -> List[dict]:
-        inputs = self.tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
+        inputs = self.tokenizer(
+            texts,
+            padding=True,
+            truncation=True,
+            max_length=128,
+            return_tensors="pt"
+        )
 
         with torch.no_grad():
             outputs = self.model(**inputs)
 
         logits = outputs.logits
-        probs = torch.sigmoid(logits)  # multi-label scenario
+        probs = torch.sigmoid(logits).cpu().numpy()  # multi-label scenario
 
         results = []
         for prob in probs:
             scores = prob.tolist()
-            
-            # Get all labels with their scores
             label_score_pairs = sorted(
                 zip(self.categories, scores), 
                 key=lambda x: x[1], 
@@ -74,7 +81,7 @@ class ClassifyWorker:
         self.collection = content_collection
         self.classifier = classifier
         self.content_kafka_service = content_kafka_service
-        self.executor = ThreadPoolExecutor(max_workers=4)
+        self.executor = ThreadPoolExecutor(max_workers=2)
 
     def add_to_buffer(self, item):
         with self.buffer_lock:
@@ -101,15 +108,21 @@ class ClassifyWorker:
             logger.info(f"Starting classify_and_update for {len(buffer)} items")
             
             texts = [f"{item['title']}. {item['desc']}" for item in buffer]
-            batch_size = 8
+            batch_size = 4
             batches = [texts[i:i + batch_size] for i in range(0, len(texts), batch_size)]
             
             results = []
+            """memory-load increases this thing can be used to speedup the process"""
             futures = [self.executor.submit(self.classifier.classify, batch) for batch in batches]
             for idx, future in enumerate(futures, start=1):
                 res = future.result()
                 logger.info(f"Batch {idx} processed")
                 results.extend(res)
+            
+            # for idx, batch in enumerate(batches, start=1):
+            #     result = self.classifier.classify(batch)  # Sequential processing
+            #     results.extend(result)
+            #     logger.info(f"Batch {idx}/{len(batches)} processed")
             
             operations = []
             batch_updates = []
